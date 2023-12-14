e3b51f6645bbe103a8b5d5811d2c1d2e7813ccb0
admin <pkstiyara+dockerbb@gmail.com>
2023-12-14 06:44:49 +0530

    Script updated

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # apache
-This repository is used for installation of apache from source code
+
+Apache Web Server Installation Script
+This Bash script automates the installation process of Apache HTTP Server along with its dependencies on Red Hat Enterprise Linux (RHEL). The script downloads, compiles, and installs the required packages, making it easier to set up Apache on your system.
+
+Prerequisites
+Ensure that your system meets the following requirements before running the script:
+
+Red Hat Enterprise Linux (RHEL)
+Access to the internet for downloading dependencies
+sudo privileges or the ability to run commands as a superuser
+Usage
+Open a terminal on your RHEL system.
+
+Download the script to a local file, for example, install_apache.sh.
+
+bash
+Copy code
+wget https://raw.githubusercontent.com/your-username/your-repo/main/install_apache.sh
+Make the script executable.
+
+bash
+Copy code
+chmod +x install_apache.sh
+Run the script.
+
+bash
+Copy code
+./install_apache.sh
+Script Explanation
+The script performs the following tasks:
+
+Define Installation Paths:
+
+prefix: Specifies the installation prefix for Apache.
+tarball_dir: Sets the directory for storing downloaded tarballs.
+Install Priority Dependencies:
+
+Installs development tools, wget, expat, expat-devel, and perl-CPAN using yum.
+Download Dependencies:
+
+Downloads the necessary tarballs for Apache, APR, APR-util, PCRE, OpenSSL, and PCRE2.
+Extract Tarballs:
+
+Extracts the downloaded tarballs in the specified tarball directory.
+Compile and Install Packages:
+
+Iterates through each package, configures, compiles, and installs them.
+The packages include APR, APR-util, PCRE, OpenSSL, PCRE2, and Apache HTTP Server.
+Conclusion:
+
+Prints a message indicating that the dependencies have been downloaded, untarred, and compiled. The installed packages are located in the specified installation prefix.
+Notes
+This script assumes a clean system and may require additional adjustments for specific environments or configurations.
+Verify that you have the necessary privileges to install packages and write to the specified installation directory.
---
+++ apache.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+#Script Author : Pankaj Singh 
+#Github: pkstiyara
+# Date : 14 Dec 2023
+
+# Define installation prefix and tarball directory
+prefix="/APPS/Apache2458"
+tarball_dir="/opt/tarball"
+
+# Install priority dependencies
+sudo yum groupinstall development tools -y
+sudo yum install wget expat expat-devel -y
+sudo yum install perl-CPAN -y
+
+# Create tarball directory if needed
+mkdir -p "$tarball_dir"
+
+# List of remaining dependencies and their URLs (adjust as needed)
+dependencies=(
+  "https://dlcdn.apache.org//apr/apr-1.7.4.tar.gz"
+  "https://dlcdn.apache.org//apr/apr-util-1.6.3.tar.gz"
+  "https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz"
+  "https://www.openssl.org/source/openssl-3.2.0.tar.gz"
+  "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.42/pcre2-10.42.tar.gz"
+  "https://dlcdn.apache.org/httpd/httpd-2.4.58.tar.gz"
+)
+
+# Download remaining dependencies to tarball directory
+for url in "${dependencies[@]}"; do
+  if [[ ! -f "$tarball_dir/${url%.*}" ]]; then
+    wget "$url" -P "$tarball_dir"
+  fi
+done
+
+# Extract all downloaded tarballs
+for tarball in "$tarball_dir"/*.tar.gz; do
+  tar -xf "$tarball" -C "$tarball_dir"
+done
+
+# Compile and install packages
+cd "$tarball_dir"
+for package in apr apr-util pcre openssl pcre2 httpd; do
+  # Extract package directory if needed
+  if [[ ! -d "$package" ]]; then
+    tar -xf "$package".tar.gz
+  fi
+
+  cd "$package"
+
+  case "$package" in
+    apr)
+      ./configure --prefix="$prefix"
+      make
+      make install
+      ;;
+    apr-util)
+      ./configure --prefix="$prefix" --with-apr="$prefix/apr"
+      make
+      make install
+      ;;
+    pcre)
+      ./configure --prefix="$prefix"
+      make
+      make install
+      ;;
+    openssl)
+      ./config -fPIC --prefix="$prefix"
+      make
+      make install
+      ;;
+    pcre2)
+      ./configure --prefix="$prefix"
+      make
+      make install
+      ;;
+    httpd)
+      ./configure --prefix="$prefix" --with-apr="$prefix/apr" --with-apr-util="$prefix/apr-util" --with-pcre="$prefix/bin/pcre2-config" --with-ssl="$prefix/openssl"
+      make
+      make install
+      ;;
+  esac
+
+  cd ..
+done
+
+cd ..
+
+echo "Dependencies downloaded, untarred, and compiled in /opt/tarball. Packages were installed to $prefix."
+
